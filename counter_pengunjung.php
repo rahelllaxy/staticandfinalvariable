6ac260a61a633e9a63c130c4efec5f969f589275
rahelllaxy <rahelllaxy@gmail.com>
2026-04-22 19:33:17 +0700

    after

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -1,19 +1,28 @@
 <?php
 
 class Pengunjung {
-
+    
     public static $jumlah = 0;
-
+    
     public function __construct() {
         self::$jumlah++;
     }
 
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+echo "Jumlah Pengunjung sebelum reset: " . Pengunjung::$jumlah . "\n";
+
+Pengunjung::reset();
+
+echo "Jumlah Pengunjung sesudah reset: " . Pengunjung::$jumlah . "\n";
 
 ?>
